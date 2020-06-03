41ec8c8500ecd9c38e4b38ab2dcc4c5904a4a9f6
Walker-Walker <awalker0@gmail.com>
2020-06-02 22:25:11 -0700

    added name array, for loop, and used for loop to iterate through the array to call the function with each iteration therby making the robot fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,14 +5,21 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+// You can also log multiple values at once like this 
+// console.log(playerName, playerAttack, playerHealth);
 
-var fight = function() {
+// console.log(enemyNames.length);
+// for(var i = 0; i < enemyNames.length; i ++) {
+//     console.log(enemyNames[i]);
+//     console.log(i);
+//     console.log(enemyNames[i] + " is at " + i + "index");
+// }
+
+
+var fight = function(enemyName) {
     // Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 // if player choses to fight, then fight
@@ -62,6 +69,9 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
  
 };
 
-fight();
+
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
 
 
