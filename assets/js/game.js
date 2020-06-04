23ca997e6de2f92e178a866c727ed627fcc7965e
Walker-Walker <awalker0@gmail.com>
2020-06-04 16:48:55 -0700

    fixed bug player-name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,18 @@
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  
+  }
+    console.log(getPlayerName);
+    return name;
+};
+
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -137,7 +150,7 @@ var startGame = function () {
 
     if (playerInfo.health > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-
+      
       var pickedEnemyObj = enemyInfo[i];
 
       pickedEnemyObj.health = randomNumber(40, 60);
